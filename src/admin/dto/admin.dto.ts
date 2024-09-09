5fdb26eb66de86fed61baf1923aca8349a4a7e91
hengage <tunecarterkillz@gmail.com>
2024-09-09 05:07:49 +0100

    security: validation pipe

--- src/admin/dto/admin.dto.ts
+++ src/admin/dto/admin.dto.ts
@@ -1,2 +1,30 @@
-export class CreateAdminDto {}
-export class UpdateAdminDto {}
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { AdminRole } from 'src/lib/constants';
+
+export class CreateAdminDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password: string;
+
+  @IsEnum(Object.values(AdminRole))
+  @IsNotEmpty()
+  role: string;
+}
+
+export class UpdateAdminDto extends PartialType(CreateAdminDto) {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import corsOptions from './config/cors';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(corsOptions);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip properties that do not have any decorators
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+      transform: true, // Automatically transform payload to DTO instance
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
