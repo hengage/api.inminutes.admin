82579b5f8c6d34301c6f2fa54cd5825d4844824f
hengage <tunecarterkillz@gmail.com>
2025-01-12 15:02:57 +0100

    refactor: admin schema - remove password field add last name and first name remove name field

--- src/admin/schema/admin.schema.ts
+++ src/admin/schema/admin.schema.ts
@@ -1,19 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import * as bcrypt from 'bcrypt';
-import { NextFunction } from 'express';
 import { AdminRole } from 'src/lib/constants';
 
 @Schema({ timestamps: true })
 export class Admin extends Document {
   @Prop({ required: true })
-  name: string;
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
 
   @Prop({ required: true, unique: true })
   email: string;
-
-  @Prop({ required: true })
-  password: string;
 
   @Prop({
     required: true,
@@ -26,10 +24,3 @@ export class Admin extends Document {
 export const AdminSchema = SchemaFactory.createForClass(Admin);
 
 AdminSchema.index({ email: 1 });
-
-AdminSchema.pre('save', async function (next: NextFunction) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
