85762cdcc6ad129463966b068fd7f9e2465904da
hengage <henrychizobaudeh@gmail.com>
2025-03-08 22:33:04 +0100

    fix(cors): include all necessary headers

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,17 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 const corsOptions: CorsOptions = {
-  // origin: function (origin, callback) {
-  //   if (ALLOWED_ORIGINS.includes(origin) || !origin) {
-  //     callback(null, true);
-  //   } else {
-  //     callback(new Error('Not allowed by CORS'));
-  //   }
-  // },
   origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Accept',
+    'Origin',
+    'X-Requested-With',
+  ],
+  exposedHeaders: ['Content-Length', 'Content-Type'],
+  credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
