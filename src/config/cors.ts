160aa4549b483edb6455fca098ffe262b0d9b5e1
hengage <henrychizobaudeh@gmail.com>
2025-03-08 12:54:54 +0100

    feat(security): enable all origins for cors, for development

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,14 +1,14 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ALLOWED_ORIGINS } from 'src/lib/constants';
 
 const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (ALLOWED_ORIGINS.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  // origin: function (origin, callback) {
+  //   if (ALLOWED_ORIGINS.includes(origin) || !origin) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  origin: '*',
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
