47f4361799484563984448202e9dbca7822df8b6
Henry Chizoba <henrychizobaudeh@gmail.com>
2025-03-10 06:54:39 +0100

    Fix/disable cors credentials (#30)
    * fix(cors): disbale credentials to allow wildcards for origin
    
    * fix(cors): include all necessary headers

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -9,9 +9,16 @@ const corsOptions: CorsOptions = {
   //   }
   // },
   origin: '*',
-  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: false,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Accept',
+    'Origin',
+    'X-Requested-With',
+  ],
+  exposedHeaders: ['Content-Length', 'Content-Type'],
+  credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
