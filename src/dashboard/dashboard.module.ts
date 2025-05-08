eb23ead2cca26fb18cd0c61574771bd83521770c
mujeeb olagunju <hardeyshinnah.mo@gmail.com>
2025-05-08 07:20:52 +0100

    fix(dashboard):  add dashboard service to providers

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -4,8 +4,7 @@ import { DashboardController } from './dashboard.controller';
 import { ApiService } from 'src/lib/apiCalls';
 
 @Module({
-  controllers: [DashboardController],
-  providers: [DashboardService, ApiService],
+  providers: [ApiService],
   exports: [ApiService]
 })
 export class DashboardModule {}
