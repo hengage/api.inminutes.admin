ae1690a592f6fabea45453a2eca9a3cb2245c297
Henry Chizoba <henrychizobaudeh@gmail.com>
2025-07-07 02:47:17 +0100

    Feat/metrics (#64)
    * feat(metrics): introduce Metrics module with controller and service for
    - introduce Metrics module
    - implement vendor metrics endpoints
    
    * feat(metrics): add riders summary and top riders endpoints to Metrics module
    
    - Implemented getRidersSummary and getTopRiders methods in MetricsService.
    - Added corresponding controller endpoints for retrieving riders summary and top riders.

---
+++ src/metrics/metrics.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, UseGuards, Get } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { MetricsService } from './metrics.service';
+
+@Controller('metrics')
+@UseGuards(AuthGuard)
+export class MetricsController {
+  constructor(private readonly metricsService: MetricsService) {}
+
+  @Get('vendors/summary')
+  async getVendorsSummary() {
+    return this.metricsService.getVendorsSummary();
+  }
+
+  @Get('vendors/top')
+  async getTopVendors() {
+    return this.metricsService.getTopVendors();
+  }
+
+  @Get('/vendors/categories/top')
+  async getTopVendorsCategories() {
+    return this.metricsService.getTopVendorsCategories();
+  }
+
+  @Get('/riders/summary')
+  async getRidersSummary() {
+    return this.metricsService.getRidersSummary();
+  }
+
+  @Get('/riders/top')
+  async getTopRiders() {
+    return this.metricsService.getTopRiders();
+  }
+}
---
+++ src/metrics/metrics.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { throwHttpException } from 'src/lib/';
+import { ApiService } from 'src/lib/apiCalls';
+
+@Injectable()
+export class MetricsService {
+  private readonly MODULE_URL = 'admin/metrics';
+
+  constructor(private readonly apiService: ApiService) {}
+
+  async getVendorsSummary() {
+    try {
+      return await this.apiService.get(`${this.MODULE_URL}/vendors/summary`);
+    } catch (error) {
+      throwHttpException(error);
+    }
+  }
+
+  async getTopVendors() {
+    try {
+      return await this.apiService.get(`${this.MODULE_URL}/vendors/top`);
+    } catch (error) {
+      throwHttpException(error);
+    }
+  }
+
+  async getTopVendorsCategories() {
+    try {
+      return await this.apiService.get(
+        `${this.MODULE_URL}/vendors/categories/top`,
+      );
+    } catch (error) {
+      throwHttpException(error);
+    }
+  }
+
+  async getRidersSummary() {
+    try {
+      return await this.apiService.get(`${this.MODULE_URL}/riders/summary`);
+    } catch (error) {
+      throwHttpException(error);
+    }
+  }
+
+  async getTopRiders() {
+    try {
+      return await this.apiService.get(`${this.MODULE_URL}/riders/top`);
+    } catch (error) {
+      throwHttpException(error);
+    }
+  }
+}
